e3d99a49a8c1fea63f25366d0ad3e46ffaca296e
stephen321 <stephen.enniis@gmail.com>
2017-02-19 13:56:18 +0000

    GJPTEAMD-58 #time 5m  some more changes made to properties and filters so Argo project can use Net code

---
+++ src/clientTest.cpp
@@ -0,0 +1,37 @@
+//#include <SDL2/SDL.h>
+//#include "Net.h"
+//
+//int main(int argc, char** argv)
+//{
+//	SDLNet_Init();
+//
+//	Net net(4000);
+//
+//	Net::ConnectData sendData;
+//	sendData.s = "hello!";
+//	sendData.ts = 100.4126f;
+//	net.Send(&sendData, "192.168.15.38", 4023);
+//
+//	bool waiting = true;
+//	while (waiting)
+//	{
+//		Net::ReceivedData receiveData = net.Receive();
+//		if (receiveData.Empty() == false)
+//		{
+//			switch (receiveData.GetType())
+//			{
+//			case Net::MessageType::Connect:
+//				Net::ConnectData* data = receiveData.GetData<Net::ConnectData>();
+//				IPaddress srcAddress = data->srcAddress;
+//				std::cout << "Received from: " << srcAddress.host << ":" << srcAddress.port << std::endl;
+//				std::cout << data->ts << std::endl;
+//				std::cout << data->s.c_str() << std::endl;
+//				waiting = false;
+//			}
+//		}
+//	};
+//
+//	system("PAUSE");
+//	SDLNet_Quit();
+//	return 0;
+//}
